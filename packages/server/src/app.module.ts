8ba3f606ec6e0405c35795ae4f300b7dfe754732
Guillaume Roy <gr.guillaume.roy@gmail.com>
2020-09-28 22:09:35 +0200

    fix(users): connect to database

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongoConnectionOptions } from 'typeorm/driver/mongodb/MongoConnectionOptions';
 import configuration from './config/configuration';
+import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -10,6 +13,17 @@ import { UsersModule } from './users/users.module';
       load: [configuration],
     }),
     UsersModule,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get('DATABASE_TYPE'),
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [User],
+        useUnifiedTopology: true,
+      }) as MongoConnectionOptions,
+    })
   ],
 })
 export class AppModule { }
--- packages/server/src/users/entities/user.entity.ts
+++ packages/server/src/users/entities/user.entity.ts
@@ -1,6 +1,19 @@
+import { Entity, ObjectID, ObjectIdColumn, Column } from "typeorm";
+
+@Entity()
 export class User {
+    @ObjectIdColumn()
+    public id: ObjectID;
+
+    @Column()
     public email: string;
+
+    @Column()
     public password: string;
+
+    @Column()
     public lastname: string;
+
+    @Column()
     public firstname: string;
 }
